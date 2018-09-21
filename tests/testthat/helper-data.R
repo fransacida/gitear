c239a13d45622e1dc38eaff974ac86635c190cd2
franciscosacida2018 <francisco.sacida@delaguayaba.com>
2018-09-21 17:06:20 -0600

    Update of R and test funtions

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,6 +1,7 @@
 library(httr)
 library(jsonlite)
 
+<<<<<<< HEAD
 base_url <- Sys.getenv("URI")
 api_key <- Sys.getenv("TOKEN")
 owner <- Sys.getenv("USUARIO")
@@ -11,3 +12,9 @@ id_org <- Sys.getenv("ID_ORG")
 
 id_hook <- Sys.getenv("ID_HOOK")
 
+=======
+base_url <- "https://try.gitea.io"
+api_key <- "token 6ebcaefdaaf06aa7f59b4efc5faa4bcf1b56cfb1"
+owner <- "fransacida"
+repo <- "abc"
+>>>>>>> Update of R and test funtions
